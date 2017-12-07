395416373d6373b9dedb7b35afafa914aa040515
Sofia Boström <sofia.bostrom@aftonbladet.se>
2017-12-07 14:07:09 +0100

    Added some images to the categories

--- code/src/components/categorygrid/categorygrid.js
+++ code/src/components/categorygrid/categorygrid.js
@@ -6,13 +6,16 @@ class Categorygrid extends React.Component {
     return (
       <div className="categories">
         <div className="category">
-          <h2>HELLO WORLD!</h2>
+          <h2 className="headingCategory">KLOCKOR!</h2>
+          <div className="imageCategory" style={{ backgroundImage: "url(./images/dandelion09_akihiro_yoshida.jpg)" }} alt="Category:Watches" />
         </div>
         <div className="category">
-          <h2>HELLO WORLD!</h2>
+          <h2 className="headingCategory" style={{ color: "#ffffff" }}>STOLAR!</h2>
+          <div className="imageCategory" style={{ backgroundImage: "url(./images/11276-8204036.jpg)" }} alt="Category:Chairs" />
         </div>
         <div className="category">
-          <h2>HELLO WORLD!</h2>
+          <h2 className="headingCategory" style={{ color: "#ffffff" }}>KONST!</h2>
+          <div className="imageCategory" style={{ backgroundImage: "url(./images/Rogue-one_issa_2.jpg)" }} alt="Category:Art" />
         </div>
       </div>
     )
