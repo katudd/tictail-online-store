cb34fdbe55863faa99e4865d9ad72f30f3c2c366
karinbellander <karinbellander@gmail.com>
2017-12-09 13:13:38 +0100

    Changes when hover

--- code/src/components/product/product.js
+++ code/src/components/product/product.js
@@ -7,7 +7,7 @@ class Product extends React.Component {
       <div className="product-box">
         <div className="product-overlay">
           <div className="product-title">{this.props.title}</div>
-          <div className="product-price">{this.props.price}</div>
+          <div className="product-price">{this.props.price} kr</div>
         </div>
         <div className="image-product" style={{ backgroundImage: `url(${this.props.image})` }} alt="name" />
       </div>
