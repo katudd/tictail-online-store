968ef887b2bd10cd04e83164b41535f6b715a661
karinbellander <karinbellander@gmail.com>
2017-12-09 12:42:13 +0100

    Added divs

--- code/src/components/product/product.js
+++ code/src/components/product/product.js
@@ -6,8 +6,8 @@ class Product extends React.Component {
     return (
       <div className="product-box">
         <div className="product-overlay">
-          {this.props.title}
-          {this.props.price}
+          <div className="product-title">{this.props.title}</div>
+          <div className="product-price">{this.props.price}</div>
         </div>
         <div className="image-product" style={{ backgroundImage: `url(${this.props.image})` }} alt="name" />
       </div>
