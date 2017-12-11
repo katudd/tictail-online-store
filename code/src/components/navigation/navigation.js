0ee53ed02b9ec4361fcc7311e34d2dc4418de6bd
katudd <katrin@uddstromer.se>
2017-12-11 16:10:36 +0100

    Mobile menu step2

--- code/src/components/navigation/navigation.js
+++ code/src/components/navigation/navigation.js
@@ -15,24 +15,22 @@ class Navigation extends React.Component {
           <label className="hamburger" htmlFor="hamburger">
             <div className="bar bar1" />
             <div className="bar bar2" />
-            <div clasName="bar bar3" />
+            <div className="bar bar3" />
           </label>
 
           <div className="menu">
 
             <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Products </a></li>
-              <li><a href="#">Products</a></li>
-
+              <Link to="/"><li>Home</li></Link>
+              <Link to="/products"><li><label className="drop-icon" htmlFor="sm">Products</label></li></Link>
               <input type="checkbox" id="sm" />
               <ul className="sub-menu">
-                <li><a href="#">Klockor</a></li>
-                <li><a href="#">Stolar</a></li>
-                <li><a href="#">Tavlor</a></li>
+                <Link to="/klockor"><li>Klockor</li></Link>
+                <Link to="/stolar"><li>Stolar</li></Link>
+                <Link to="/tavlor"><li>Tavlor</li></Link>
               </ul>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Newsletter</a></li>
+              <Link to="/about"><li>About</li></Link>
+              <Link to="/newsletter"><li>Newsletter</li></Link>
             </ul>
           </div>
         </div>
