48e093686d2988fddd9ca4f3ca3d5dbca95cd573
Sofia Boström <sofia.bostrom@aftonbladet.se>
2017-12-11 16:19:45 +0100

    Added the "Account"-format to price

--- code/src/components/product/product.js
+++ code/src/components/product/product.js
@@ -1,4 +1,5 @@
 import React from "react"
+import accounting from "accounting"
 import "./product.css"
 
 class Product extends React.Component {
@@ -7,7 +8,7 @@ class Product extends React.Component {
       <div className="product-box">
         {/* <div className="product-overlay"> */}
           <div className="product-title">{this.props.title}</div>
-          <div className="product-price">{this.props.price} kr</div>
+          <div className="product-price">{accounting.formatMoney(this.props.price / 100)} kr</div>
         {/* </div> */}
         <div className="image-product" style={{ backgroundImage: `url(${this.props.image})` }} alt="name" />
       </div>
