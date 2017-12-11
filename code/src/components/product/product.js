464f1b1100f3f6666e6a82b2505d71d03ef6c3e1
karinbellander <karinbellander@gmail.com>
2017-12-11 14:22:40 +0100

    Changed the hover boxes products

--- code/src/components/product/product.js
+++ code/src/components/product/product.js
@@ -5,10 +5,10 @@ class Product extends React.Component {
   render() {
     return (
       <div className="product-box">
-        <div className="product-overlay">
+        {/* <div className="product-overlay"> */}
           <div className="product-title">{this.props.title}</div>
           <div className="product-price">{this.props.price} kr</div>
-        </div>
+        {/* </div> */}
         <div className="image-product" style={{ backgroundImage: `url(${this.props.image})` }} alt="name" />
       </div>
     )
