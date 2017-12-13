2ef7a50ced5def68f64fb24f02984d4157e61619
nicolepehar <nicole.pehar@gmail.com>
2017-12-13 22:59:36 +0100

    focus

--- code/src/components/logo/logo.js
+++ code/src/components/logo/logo.js
@@ -2,11 +2,11 @@ import React from "react"
 import "./logo.css"
 
 import logo from "../../images/homebound_logo_green.svg"
-import house from "../../images/houseorange.png"
+import house from "../../images/housecircle.png"
 import art from "../../images/art.png"
-import chairs from "../../images/chair_1.png"
-import clocks from "../../images/watch.png"
-import products from "../../images/products_1.png"
+import chairs from "../../images/chairdarkgreen.png"
+import clocks from "../../images/clockslightgreen_1.png"
+import products from "../../images/productsdarkgreen.png"
 import PropTypes from 'prop-types';
 
 class Logo extends React.Component {
