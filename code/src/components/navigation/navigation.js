54daa5a1da887dea0a15c98b0f98394311983895
katudd <katrin@uddstromer.se>
2017-12-13 10:33:40 +0100

    Changed navgation added filter

--- code/src/components/navigation/navigation.js
+++ code/src/components/navigation/navigation.js
@@ -3,7 +3,44 @@ import React from "react"
 import "./navigation.css"
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      categories: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.tictail.com/v1.26/stores/5znw/categories").then(response =>
+      response.json()).then(json =>
+      this.setState({
+        categories: json
+      }))
+  }
+
+  renderNavigation() {
+    const categories = this.state.categories
+    const items = categories.map((category) => {
+      return (
+        <li key={category.id}><Link to={`/category/${category.slug}`} className="links sub-menu">{category.title}</Link></li>
+      )
+    })
+
+    return (
+      <ul>
+        <li><Link to="/" className="links">Home</Link></li>
+        <li><Link to="/products" className="links">Products</Link></li>
+        <ul className="sub-menu">
+          {items}
+        </ul>
+        <li><Link to="/about" className="links">About</Link></li>
+        <li><Link to="/newsletter" className="links">Contact</Link></li>
+      </ul>
+    )
+  }
+
   render() {
+    console.log('this.state', this.state)
     return (
 
       <nav id="menu">
@@ -19,19 +56,7 @@ class Navigation extends React.Component {
           </label>
 
           <div className="menu">
-
-            <ul>
-              <li><Link to="/" className="links">Home</Link></li>
-              <li><Link to="/products" className="links">Products</Link></li>
-              {/* <ul className="sub-menu"> */}
-              <li><Link to="/klockor" className="links sub-menu">Clocks</Link></li>
-              <li><Link to="/stolar" className="links sub-menu">Chairs</Link></li>
-              <li><Link to="/tavlor" className="links sub-menu">Art</Link></li>
-              {/* </ul> */}
-              <li><Link to="/about" className="links">About</Link></li>
-              <li><Link to="/newsletter" className="links">Contact</Link></li>
-            </ul>
-
+            {this.renderNavigation()}
           </div>
         </div>
       </nav>
